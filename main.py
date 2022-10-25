2ef22ea206f916e9b6f94f18563b298e021a5c4f
ivkrak <55966557+IKrakovskii@users.noreply.github.com>
2022-10-25 13:12:37 +0800

    Создан массив с рандомными числами

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# n = int(input('Введите значение n - '))
+# a = int(input('Введите значение a - '))
+# b = int(input('Введите значение b - '))
+
+n = 5
+a = -60
+b = 60
+
+
+arr = np.random.randint(low=min(a, b), high=max(a, b), size=(n, n))
+
+print(arr)
