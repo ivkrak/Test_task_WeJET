3136f45f59f67b204038bdb3eaf61306c877cd4b
ivkrak <iv.krak@gmail.com>
2022-10-26 00:09:37 +0800

    Пререлиз, остались только тесты))

--- functions.py
+++ functions.py
@@ -23,7 +23,7 @@ def random_array(low, high, size):
     return np.random.randint(low=low, high=high, size=(size, size))
 
 
-def print_array(arr):
+def str_array(arr):
     """
     **Красиво выводит двумерный массив**
 
@@ -31,7 +31,51 @@ def print_array(arr):
     """
     string_arr = str(arr)[1:-1].replace('[', '|')
     string_arr = ' ' + string_arr.replace(']', '|')
-    print(string_arr)
+    return string_arr
+
+
+def sum_of_first_triangle(arr):
+    triangle_1 = []
+    shape = arr.shape[0]
+    a = 0
+    b = int(shape)
+    for j in range(int(shape / 2) if shape % 2 == 0 else int(shape / 2) + 1):
+        for i in range(a, b):
+            triangle_1.append(arr[j][i])
+            # print(arr[j][i])
+        a += 1
+        b -= 1
+    return sum(triangle_1)
+
+
+def sum_of_third_triangle(arr):
+    arr = arr.transpose()
+    return sum_of_first_triangle(arr)
+
+
+def sum_of_fourth_triangle(arr):
+    arr = arr.transpose()
+    return sum_of_second_triangle(arr)
+
+
+def sum_of_second_triangle(arr):
+    triangle_2 = []
+    shape = arr.shape[0]
+    if shape % 2 == 0:
+        b = int(shape / 2) + 1
+        a = int(shape / 2) - 1
+        x = int(shape / 2)
+    else:
+        b = int(shape / 2) + 1
+        a = int(shape / 2)
+        x = int(shape / 2)
+
+    for j in range(x, shape):
+        for i in range(a, b):
+            triangle_2.append(arr[j][i])
+        a -= 1
+        b += 1
+    return sum(triangle_2)
 
 
 def sum_of_numbers_in_a_triangle(arr):
@@ -41,4 +85,8 @@ def sum_of_numbers_in_a_triangle(arr):
     * принимает на вход массив numpyArray
     * выводит список с суммами треугольников -> [сумма I △, сумма II △, сумма III △, сумма IV △]
     """
-    pass
+    out = f'Первый треугольник: {sum_of_first_triangle(arr)}\n' \
+          + f'Второй треугольник: {sum_of_second_triangle(arr)}\n' \
+          + f'Третий треугольник: {sum_of_third_triangle(arr)}\n' \
+          + f'Четвертый треугольник: {sum_of_fourth_triangle(arr)}'
+    return out
--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 from functions import *
 
-triangle_1 = []
-# n = int(input('Введите значение n - '))
-# a, b = array_random_numbers()
-# while a >= b:
-#     print('ERROR!\n\nПовторите ввод чисел a, b\n\nЧисло a не должно превышать число b!\n')
-#     a, b = array_random_numbers()
+if __name__ == "__main__":
+    # n = int(input('Введите значение n - '))
+    # a, b = array_random_numbers()
+    # while a >= b:
+    #     print('ERROR!\n\nПовторите ввод чисел a, b\n\nЧисло a не должно превышать число b!\n')
+    #     a, b = array_random_numbers()
 
-# Проверочные данные
-# n = 5
-# a = -60
-# b = 60
+    # Проверочные данные
+    n = 5
+    a = -60
+    b = 60
 
-# arr = random_array(low=min(a, b), high=max(a, b), size=n)
+    arr = np.array([[56, 43, 45, 45, 34],
+                    [-56, 45, -6, 5, 6],
+                    [5, -6, 4, 6, 6],
+                    [45, 6, -8, 5, 1],
+                    [0, 7, 8, 5, 7]])
 
-arr = np.array([[56, 43, 45, 45, 34],
-                [-56, 45, -6, 5, 6],
-                [5, -6, 4, 6, 6],
-                [45, 6, -8, 5, 1],
-                [0, 7, 8, 5, 7]])
+    print(f'Сгенерированный массив:\n {arr}')
 
-print_array(arr)
-
-shape = arr.shape[0]
-print(f'shape: {shape}')
-a = 0
-b = int(shape)
-for j in range(3):
-    for i in range(a, b):
-        triangle_1.append(arr[j][i])
-        print(arr[j][i])
-    a += 1
-    b -= 1
-print(f'Первый треугольник: {sum(triangle_1)}')
+    print(sum_of_numbers_in_a_triangle(arr))
