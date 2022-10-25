3b72d08b153c18d606d3dfc03742739e20e20f49
ivkrak <iv.krak@gmail.com>
2022-10-25 22:56:52 +0800

    Заработал Первый треугольник!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions import *
 
+triangle_1 = []
 # n = int(input('Введите значение n - '))
 # a, b = array_random_numbers()
 # while a >= b:
@@ -14,21 +15,21 @@ from functions import *
 # arr = random_array(low=min(a, b), high=max(a, b), size=n)
 
 arr = np.array([[56, 43, 45, 45, 34],
-        [-56, 45, -6, 5, 6],
-        [5, -6, 4, 6, 6],
-        [45, 6, -8, 5, 1],
-        [0, 7, 8, 5, 7]])
+                [-56, 45, -6, 5, 6],
+                [5, -6, 4, 6, 6],
+                [45, 6, -8, 5, 1],
+                [0, 7, 8, 5, 7]])
 
 print_array(arr)
 
 shape = arr.shape[0]
 print(f'shape: {shape}')
-a, b = 0, shape
+a = 0
+b = int(shape)
 for j in range(3):
-    print(f'j: {j}')
     for i in range(a, b):
-        print('__________________')
-        print(arr[::][i])
-        a += 1
-        b -= 1
-
+        triangle_1.append(arr[j][i])
+        print(arr[j][i])
+    a += 1
+    b -= 1
+print(f'Первый треугольник: {sum(triangle_1)}')
